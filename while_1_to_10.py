0b5fc03d7e93ac88144d2ebb2160e798ab79acf7
Ni4ka <astahova_nica@mail.ru>
2021-03-18 11:27:59 +0200

    Add file while_1_to_10.py

---
+++ while_1_to_10.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env Python3
+from os import system
+
+
+system("clear")
+
+#DATA
+
+start_n = int( input( "Enter start number:\n" )) 
+end_n   = int( input( "Enter end number:\n" ))
+
+
+
+#LOGIC
+
+if ( start_n < end_n ):
+ while ( start_n <= end_n ):
+  print( start_n )
+  start_n += 1
+
+elif( start_n > end_n ):
+ while ( end_n <= start_n ):
+  print( end_n )
+  end_n += 1
+elif( start_n == end_n ):
+ print("Error. You entered the same numbers.\n")
